a8108333f57502070de6e74c0eccf5204f486588
Mark Pothier <mark.d.pothier@gmail.com>
2018-06-03 00:24:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ function takeANumber(lineList, name) {
   lineList.push(name)
   return `Welcome, ${name}. You are number ${lineList.length} in line.`
 }
+
+function nowServing(lineList) {
+  if (lineList.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+  else {
+    return lineList.shift()
+  }
+}
